2e61c272a0b50757840c2cc996bdb11f6a855f33
Amrampro <gouliben@gmail.com>
2025-06-16 09:56:29 +0200

    footer edited

--- resources/views/layouts/charifund_template/footer.blade.php
+++ resources/views/layouts/charifund_template/footer.blade.php
@@ -7,16 +7,17 @@
                         <div class="row align-items-center justify-content-center gutter-30">
                             <div class="col-12 col-sm-8 col-lg-5 col-xl-6">
                                 <div class="footer__content">
-                                    <h3 class="title-animation">children need your help
-                                        start <span>donating</span> today
+                                    <h3 class="title-animation">
+                                        Femmes Interface Nord-Sud <br>
+                                        <i><span>Women Interface North South</span></i>
                                     </h3>
                                 </div>
                             </div>
                             <div class="col-12 col-lg-7 col-xl-6">
                                 <div class="footer__support" data-aos="fade-up" data-aos-duration="1000"
-                                     data-aos-delay="300">
-                                    <a href="contact-us.html">
-                                        for Any Inquiry <span>@WOWtheme.co</span>
+                                    data-aos-delay="300">
+                                    <a href="/contact">
+                                        Pour toute demande <span>Fins-Wins</span>
                                     </a>
                                 </div>
                             </div>
@@ -28,52 +29,48 @@
                 <div class="col-12 col-xl-6">
                     <div class="footer__newsletter" data-aos="fade-up" data-aos-duration="1000">
                         <div class="footer__newsletter-info">
-                            <h4 class="title-animation">Subscribe Newsletter</h4>
-                            <p>We understand that every challenge is an opportunity </p>
+                            <h4 class="title-animation">S'abonner à la Newsletter</h4>
+                            <p>Nous comprenons que chaque défi est une opportunité</p>
                         </div>
                         <div class="footer__newsletter-form">
                             <form action="#" method="post">
                                 <div class="input-icon">
                                     <input type="email" required name="subscribe-email" id="subscribeEmail"
-                                           placeholder="Enter Email">
+                                        placeholder="Email">
                                     <i class="fa-regular fa-envelope"></i>
                                 </div>
                                 <button type="submit" aria-label="subscribe to our newsletter"
-                                        title="subscribe to our newsletter" class="btn--primary">Subscribe <i
+                                    title="subscribe to our newsletter" class="btn--primary">Envoyer <i
                                         class="fa-solid fa-paper-plane"></i>
                                 </button>
                             </form>
                         </div>
-                        <div class="footer__newsletter-check">
+                        {{-- <div class="footer__newsletter-check">
                             <div class="form-group">
                                 <input type="checkbox" id="acceptPolicy">
                                 <label for="acceptPolicy">By subscribing, you're accepting our <a
                                         href="contact-us.html">Privacy Policy</a></label>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
                 <div class="col-12 col-md-6 col-xl-2 offset-xl-1">
                     <div class="footer__list" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
                         <div class="footer__list-intro">
-                            <h5 class="title-animation">Services</h5>
+                            <h5 class="title-animation">Liens</h5>
                         </div>
                         <div class="footer__list-items">
                             <ul>
-                                <li><a href="about-us.html"><i class="fa-solid fa-angles-right"></i>About Us</a>
+                                <li><a href="about-us.html"><i class="fa-solid fa-angles-right"></i>Accueil</a>
                                 </li>
-                                <li><a href="our-causes.html"><i class="fa-solid fa-angles-right"></i>Our
-                                        Causes</a>
+                                <li><a href="our-causes.html"><i class="fa-solid fa-angles-right"></i>A propos</a>
                                 </li>
-                                <li><a href="our-team.html"><i class="fa-solid fa-angles-right"></i>Our
-                                        Teams</a>
+                                <li><a href="our-team.html"><i class="fa-solid fa-angles-right"></i>Actions</a>
                                 </li>
-                                <li><a href="blog-list.html"><i class="fa-solid fa-angles-right"></i>Our
-                                        Blog</a>
+                                <li><a href="blog-list.html"><i class="fa-solid fa-angles-right"></i>Photos</a>
                                 </li>
-                                <li><a href="faq.html"><i class="fa-solid fa-angles-right"></i>FAQ</a></li>
-                                <li><a href="contact-us.html"><i class="fa-solid fa-angles-right"></i>Get A
-                                        Quote</a>
+                                <li><a href="faq.html"><i class="fa-solid fa-angles-right"></i>Agenda</a></li>
+                                <li><a href="contact-us.html"><i class="fa-solid fa-angles-right"></i>Contact</a>
                                 </li>
                             </ul>
                         </div>
@@ -103,6 +100,8 @@
                                 </p>
                                 <p><a href="tel:+32485150430"> +32 (0) 485 15 04 30</a>
                                 </p>
+                                <p><a href="tel:+32485150430"> +32 (0) 10 88 10 53</a>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -123,17 +122,17 @@
                                     </div>
                                     <div class="col-12 col-xl-6">
                                         <div class="footer__bottom-right text-center">
-                                            <p>Copyright &copy; <span id="copyrightYear"></span> <a href="index.html"
-                                                                                                    target="_blank">Charifund</a>. All rights
-                                                reserved.
+                                            <p>Copyright &copy; <span id="copyrightYear"></span> <a
+                                                    href="/">Fins-Wins</a>. Tous droits reservés
                                             </p>
                                         </div>
                                     </div>
                                     <div class="col-12 col-xl-3">
                                         <div class="footer__bottom-left">
-                                            <ul class="footer__bottom-list justify-content-center justify-content-xl-end">
-                                                <li><a href="terms-conditions.html">Terms & Conditions</a></li>
-                                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                            <ul
+                                                class="footer__bottom-list justify-content-center justify-content-xl-end">
+                                                <li><i>Designed & Developed by </i><a
+                                                        href="https://www.goulbam.com/">GoulBAM Enterprises</a></li>
                                             </ul>
                                         </div>
                                     </div>
